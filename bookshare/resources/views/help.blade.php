b1e7c633f9019823d47f478c370182c2752eb5db
Jonathan Cromie <joncromie@gmail.com>
2015-09-22 12:38:18 +1000

    Merge branch jcromie

---
+++ bookshare/resources/views/help.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('title', 'Sign Up')
+
+@section('content')
+	@parent
+
+	<h2>Help Page</h2>
+
+	<h1>How to share books?</h1>
+  	<p> This section is to explain the steps using pics</p>
+
+  	<h1> How to search by location?</h1>
+  	<p> This section is to explain the steps using pics, other tut will follow </p>
+
+@endsection
---
+++ bookshare/resources/views/login.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('title', 'Login')
+
+@section('content')
+    @parent
+    <h2>Login</h2>
+	<form action="pro.php" method="post">
+        <fieldset>
+        <legend> Registered Users</legend>
+		User name: <input type="text" name="username"/> <br/>
+		Password: <input type="password" name="password"/> <br/>	
+		<input type="submit" name="login" value="Login"/>
+		</fieldset>
+    </form>
+
+    <p> Not registered yet? <a href="signup">Click here</a> </p>
+@endsection
