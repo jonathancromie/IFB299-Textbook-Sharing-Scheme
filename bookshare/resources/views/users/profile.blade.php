0b1fe08ff9011317ca18c7792a341ba84d34d45d
jonathancromie <joncromie@gmail.com>
2015-10-26 16:50:11 +1000

    Updated profile design

--- bookshare/resources/views/users/profile.blade.php
+++ bookshare/resources/views/users/profile.blade.php
@@ -2,8 +2,40 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="{{ asset('js/profile-tabs.js') }}"></script>
-<script src="{{ asset('js/profile-rating.js') }}"></script>
+<!-- <script src="{{ asset('js/profile-rating.js') }}"></script> -->
+<script src="js/jquery-profile.min.js" type="text/javascript"></script>
 <link rel="stylesheet" href="{{ asset('css/profilestyle.css') }}" type="text/css" />
+
+<script type="text/javascript">(function(d, t, e, m){
+    
+    // Async Rating-Widget initialization.
+    window.RW_Async_Init = function(){
+                
+        RW.init({
+            huid: "265016",
+            uid: "54cf6b439223e4998e990d50a81b0b1d",
+            source: "website",
+            options: {
+                "size": "medium",
+                "style": "oxygen",
+                "isDummy": false
+            } 
+        });
+        RW.render();
+    };
+        // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+        l = d.location, ck = "Y" + t.getFullYear() + 
+        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+    if (d.getElementById(id)) return;              
+    rw = d.createElement(e);
+    rw.id = id; rw.async = true; rw.type = "text/javascript";
+    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+    s.parentNode.insertBefore(rw, s);
+    }(document, new Date(), "script", "rating-widget.com/"));
+</script>
 
  <!--<script src="{{ asset('js/jquery-profile.min.js') }}"></script> -->
 
@@ -14,46 +46,32 @@
     <h2>Profile</h2> 
 
     <img src="http://community.nasdaq.com/common/images/defaultUserAvatar.jpg" alt="default avatar">  <!-- src="img/avatar.png -->  
-    <table>
-      <thead>
-        <tr>
-          <td><a href="#bio" class="sel">Bio</a></td>
-          <td><a href="#activity">Contact Information</a></td>
-          <td><a href="#friends">Shared Books</a></td>
-          <td><a href="#settings">Borrowed Books</a></td>
-        </tr>
-      </thead>
-    </table>  
+    
+    <div id="w">
+      <div id="content" class="clearfix">
+       <nav id="profiletabs">
+            <ul class="clearfix">
+              <li><a href="#bio" class="sel">Bio</a></li>
+              <li><a href="#activity">Contact Information</a></li>
+              <li><a href="#friends">Shared Books</a></li>
+              <li><a href="#settings">Borrowed Books</a></li>
+              <!-- <li><a href="#settings"> Feedback </a></li> -->
+            </ul>
+        </nav>
         
       <!-- Tab 1 : basic info + rating -->    
         <section id="bio">    
               <!-- Basic info-->
-          <p class="setting"><span> Given Name </span> {{ Auth::user()->first_name }} </p>
-          
-          <p class="setting"><span> Surname </span> {{ Auth::user()->last_name }} </p>
-          
-          <p class="setting"><span>Sex </span> {{ Auth::user()->sex }} </p>
-          
-          <p class="setting"><span>Date of Birth </span> {{ Auth::user()->dob }} </p>
-          
-          <p class="setting"><span>Edit <img src="img/edit.png" alt="*Edit*"></span> <a class="btn btn-small btn-success" href="{{ URL::to('user/profile') }}">Edit</a> </p>
-
-    			</br>      
-            <!-- Star Rating -->
-  <div class="rw-ui-container"></div>
-
-            <!-- feedback  -->
-           <!-- begin wwww.htmlcommentbox.com -->
-         <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Comment Form</a> is loading comments...</div>
-           <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
-           <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&opts=16862&num=10&ts=1445797376758");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
-			<!-- end www.htmlcommentbox.com -->
-            
-        </section>
-      
+          <p class="setting"><span>Given Name </span> {{ Auth::user()->first_name }} </p>          
+          <p class="setting"><span>Surname </span> {{ Auth::user()->last_name }} </p>          
+          <p class="setting"><span>Sex </span> {{ Auth::user()->sex }} </p>          
+          <p class="setting"><span>Date of Birth </span> {{ Auth::user()->dob }} </p>          
+          <!-- <p class="setting"><span>Edit <img src="img/edit.png" alt="*Edit*"></span> <a class="btn btn-small btn-success" href="{{ URL::to('user/profile') }}">Edit</a> </p> -->
+    			</br>              
+        </section>      
         <!-- Tab 2 : Contact Information -->        
         <section id="activity" class="hidden">
-          <table border="1" style="width:100%">
+          <!-- <table border="1" style="width:100%">
             <tr>
               <td>Email Address</td>
               <td>{{ Auth::user()->email }}</td>		
@@ -78,11 +96,21 @@
               <td>State</td>
               <td>{{ Auth::user()->state }}</td>		
             </tr>
-          </table>
-          <p class="setting"><span>Edit <img src="img/edit.png" alt="*Edit*"></span> <a class="btn btn-small btn-success" href="{{ URL::to('user/profile') }}">Edit</a> </p>
+          </table> -->
+          <p class="setting"><span>Email Address </span> {{ Auth::user()->email }} </p>          
+          <p class="setting"><span>Phone </span> {{ Auth::user()->phone }} </p>          
+          <p class="setting"><span>Street </span> {{ Auth::user()->street }} </p>          
+          <p class="setting"><span>Suburb </span> {{ Auth::user()->suburb }} </p> 
+          <p class="setting"><span>Post Code </span> {{ Auth::user()->post_code }} </p>          
+          <p class="setting"><span>State </span> {{ Auth::user()->state }} </p>           
+          <!-- <p class="setting"><span>Edit <img src="img/edit.png" alt="*Edit*"></span> <a class="btn btn-small btn-success" href="{{ URL::to('user/profile') }}">Edit</a> </p> -->
         </section>
       
         <!-- Tab 3: Shared Books -->    
+
+          
+
+
         <section id="friends" class="hidden">
           <table>
               <thead> 
