f2237e462bbac3db6bbf5dc520352c59174fca2b
yaman-alsahfi <lodymoon@gmail.com>
2015-10-26 11:47:20 +1000

    edit validation

--- bookshare/resources/views/share.blade.php
+++ bookshare/resources/views/share.blade.php
@@ -5,9 +5,6 @@
 <script src="{{ asset('js/isbn.js') }}"></script>
 <script src="{{ asset('js/hyphenate.js') }}"></script>
 <script src="{{ asset('js/author.js') }}"></script>
-<!-- Simple Profile -->
-  <link rel="stylesheet" href="{{ asset('css/profilestyle.css') }}" type="text/css" /> 
-  <script src="{{ asset('js/jquery-profile.min.js') }}"></script>
 
 
 <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" type="text/css" />
@@ -17,39 +14,51 @@
 
         <link rel="stylesheet" type="text/css" href="stylesheets/jquery.validate.css" />
         <link rel="stylesheet" type="text/css" href="stylesheets/style.css" />
-
-	<script type="text/javascript">
-        
+	<!-- validate input -->
+<script type="text/javascript">
             /* <![CDATA[ */
             jQuery(function(){
-                jQuery("#ValidField").validate({
+                jQuery("#ValidField").validate({ 
                     expression: "if (VAL) return true; else return false;",
                     message: "Please enter the Required field"
                 });
-                jQuery("#ValidNumber").validate({
+                jQuery("#author").validate({ 
+                    expression: "if (VAL) return true; else return false;",
+                    message: "Please enter the Required field"
+                });
+                jQuery("#location").validate({ 
+                    expression: "if (VAL) return true; else return false;",
+                    message: "Please enter the Required field"
+                });
+                jQuery("#publisher").validate({ 
+                    expression: "if (VAL) return true; else return false;",
+                    message: "Please enter the Required field"
+                });
+                jQuery("#edition").validate({ 
+                    expression: "if (VAL) return true; else return false;",
+                    message: "Please enter the Required field"
+                });                
+                jQuery("#isbn").validate({
                     expression: "if (!isNaN(VAL) && VAL) return true; else return false;",
                     message: "Please enter a valid number"
                 });
-                jQuery("#phone").validate({
-                    expression: "if (!isNaN(VAL) && VAL) return true; else return false;",
-                    message: "Please enter a valid number"
+                jQuery("#ValidDate").validate({
+                    expression: "if (!isValidDate(parseInt(VAL.split('-')[2]), parseInt(VAL.split('-')[0]), parseInt(VAL.split('-')[1]))) return false; else return true;",
+                    message: "Please enter a valid Date"
                 });
-                jQuery("#Email").validate({
-                    expression: "if (VAL.match(/^[^\\W][a-zA-Z0-9\\_\\-\\.]+([a-zA-Z0-9\\_\\-\\.]+)*\\@[a-zA-Z0-9_]+(\\.[a-zA-Z0-9_]+)*\\.[a-zA-Z]{2,4}$/)) return true; else return false;",
-                    message: "Please enter a valid Email ID"
-                });
-                jQuery("#ValidConfirmPassword").validate({
-                    expression: "if ((VAL == jQuery('#password').val()) && VAL) return true; else return false;",
-                    message: "Confirm password field doesn't match the password field"
+                jQuery("#datetimepicker1").validate({
+                    expression: "if (!isValidDate(parseInt(VAL.split('-')[2]), parseInt(VAL.split('-')[0]), parseInt(VAL.split('-')[1]))) return false; else return true;",
+                    message: "Please enter a valid Date"
+                });               
+                jQuery("#datetimepicker2").validate({
+                    expression: "if (!isValidDate(parseInt(VAL.split('-')[2]), parseInt(VAL.split('-')[0]), parseInt(VAL.split('-')[1]))) return false; else return true;",
+                    message: "Please enter a valid Date"
                 });
                 jQuery("#ValidSelection").validate({
                     expression: "if (VAL != '0') return true; else return false;",
                     message: "Please make a selection"
                 });
-                jQuery("#state").validate({
-                    expression: "if (VAL != '0') return true; else return false;",
-                    message: "Please make a selection"
-                });
+
 				jQuery('.AdvancedForm').validated(function(){
 					alert("Use this call to make AJAX submissions.");
 				});
@@ -113,8 +122,42 @@
 <script src="{{ asset('js/bootstrap-datetimepicker.min.js') }}"></script>
 <script src="{{ asset('js/datetimepicker.js') }}"></script>
 
-<!-- validation -->
- <script type="text/javascript">
+
+<form method="POST" action="books" accept-charset="UTF-8" class="AdvancedForm" enctype="multipart/form-data">
+			<input type="text" name="name" id="ValidField"  placeholder="Name" required><br>
+			<input type="text" id="author" name="author" id="ValidField" placeholder="Author" required><br> <!-- can be deleted? -->
+			<span id="edit" onclick="edit();"></span><br>
+			<span id="isbnspan"></span><br>
+			<input type="text" id="isbn" name="isbn" placeholder="ISBN" required><br> <!-- changed id -->
+			<input type="text" id="publisher" name="publisher" placeholder="Publisher" required><br>
+			<input type="text" id="edition" name="edition" placeholder="Edition" required><br>
+			<select name="faculty" id="ValidSelection" required>
+			    <option value="0">Make a Selection</option>
+				<option value="Buidling and Planning">Building and Planning</option>
+				<option value="Business">Business</option>
+				<option value="Creative, Design and Performance">Creative, Design and Performance</option>
+				<option value="Education">Education</option>
+				<option value="Engineering">Engineering</option>
+				<option value="Health and Community">Health and Community</option>
+				<option value="Information Technology">Information Technology</option>
+				<option value="Languages">Languages</option>
+				<option value="Law and Justice">Law and Justice</option>
+				<option value="Science and Mathematics">Science and Mathematics</option>
+			</select>	
+			<br>
+	        <input type="text" class="form-control" id="datetimepicker1" name="pickup_date" placeholder="Pickup Date" required><br>		    
+			<input type="text" name="location" id="location" placeholder="Pickup Location" required><br>
+			<input type="text" class="form-control" id="datetimepicker2" name="due_date" placeholder="Due Date" required><br>
+			<input type="file" class="file" name="image" id="image" accept="image/gif, image/jpeg, image/png" required><br>
+			<input type="hidden" name="MAX_FILE_SIZE" value="20971520">
+			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+			<input type="submit" value="Submit" onclick="edit();">
+		</form>
+            
+
+            
+        </div>
+        <script type="text/javascript">
             /* <![CDATA[ */
             var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
             document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
